80333aebc301c9a38faab33d55ec8cd72fff009e
wangjx <wangjxah@gmail.com>
2019-09-06 14:17:41 +0800

    你看这个会不会冲突

---
+++ gmall-common/src/main/java/com/atguigu/utils/TestGit.java
@@ -0,0 +1,9 @@
+package com.atguigu.utils;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Git测试");
+        System.out.println("你看这个冲不冲突");
+
+    }
+}
